987e9483c0d5bfc371f13ab92774aef312f60d7d
Braian Alejandro Ledesma <braian.ledesma-99@hotmail.com>
2020-09-07 00:44:08 -0300

    Add Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids.Characters.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("General")]
+
+        [SerializeField]
+        private new Rigidbody2D rigidbody2D = null;
+
+        private void FixedUpdate()
+        {
+            Rotate();
+        }
+
+        private void Rotate()
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector2 dir = mousePosition - rigidbody2D.position;
+
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+            rigidbody2D.MoveRotation(angle);
+        }
+    }
+}
