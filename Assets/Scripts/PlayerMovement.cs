67bb6b5806de02ec9a47ae87ee54e195460239f7
Enderlook <enderlook@gmail.com>
2020-09-28 10:15:21 -0300

    Tweak PlayerMovement to make it similar to original Asteroids game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Asteroids.Characters.Player
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerMovement : MonoBehaviour
     {
-        [Header("General")]
+#pragma warning disable CS0649
+        [SerializeField, Tooltip("Acceleration per second.")]
+        private float accelerationSpeed;
 
-        [SerializeField]
-        private new Rigidbody2D rigidbody2D = null;
+        [SerializeField, Tooltip("Maximum allowed speed.")]
+        private float maximumSpeed;
+
+        [SerializeField, Tooltip("Angles rotated per second.")]
+        private float rotationSpeed;
+#pragma warning restore CS0649
+
+        private new Rigidbody2D rigidbody;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Awake() => rigidbody = GetComponent<Rigidbody2D>();
 
         private void FixedUpdate()
         {
+            Move();
             Rotate();
         }
 
-        private void Rotate()
+        private void Move()
         {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            Vector2 dir = mousePosition - rigidbody2D.position;
-
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
-            rigidbody2D.MoveRotation(angle);
+            rigidbody.AddRelativeForce(Vector2.up * Mathf.Max(Input.GetAxis("Vertical"), 0) * accelerationSpeed, ForceMode2D.Force);
+            rigidbody.velocity = Vector2.ClampMagnitude(rigidbody.velocity, maximumSpeed);
         }
+
+        private void Rotate() => rigidbody.SetRotation(rigidbody.rotation - Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
     }
 }
