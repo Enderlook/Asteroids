0308c37e0c0a6f9bea2d3f46204668b0a745bb43
Enderlook <enderlook@gmail.com>
2020-10-01 16:49:19 -0300

    Refactor and reorganice

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Asteroids.UI
+{
+    public class Pause : MonoBehaviour
+    {
+        private ScreenManager screenManager;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Awake() => screenManager = GetComponent<ScreenManager>();
+
+        public void TogglePause()
+        {
+            if (screenManager.isScreenOn)
+                Time.timeScale = 1;
+            else
+                Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ScreenManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace Asteroids.UI
+{
+    public class ScreenManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject screen;
+
+        [NonSerialized]
+        public bool isScreenOn;
+
+        public void TogglePauseMenu()
+        {
+            if (screen != null)
+            {
+                screen.SetActive(!screen.activeSelf);
+                isScreenOn = !isScreenOn;
+            }
+        }
+    }
+}
