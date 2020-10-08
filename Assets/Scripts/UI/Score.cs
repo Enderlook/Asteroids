ae5a62cc485afabcc15860efdc8405481229ff34
Enderlook <enderlook@gmail.com>
2020-10-08 13:49:55 -0300

    Add score meter

---
+++ Assets/Scripts/UI/Score.cs
@@ -0,0 +1,42 @@
+﻿using Asteroids.Events;
+using Asteroids.Scene;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Asteroids.UI
+{
+    [RequireComponent(typeof(Text))]
+    public class Score : MonoBehaviour
+    {
+        private Text text;
+        private int lastValue;
+        private float current;
+        private int target;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Awake()
+        {
+            text = GetComponent<Text>();
+            EventManager.Subscribe<ScoreHasChangedEvent>(OnScoreHasChanged);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Update()
+        {
+            if (current != target)
+            {
+                current = Mathf.MoveTowards(current, target, Time.deltaTime * (5 + target - current));
+                if (current != lastValue)
+                {
+                    lastValue = (int)current;
+                    text.text = lastValue.ToString();
+                }
+            }
+        }
+
+        private void OnScoreHasChanged(ScoreHasChangedEvent @event) => target = @event.NewScore;
+    }
+}
