ff97c623e64e535039c4b1012a81616ca404f578
Braian Alejandro Ledesma <braian.ledesma-99@hotmail.com>
2020-11-19 21:56:07 -0300

    Add IEvent interface to Avalon Studios packages

---
+++ Assets/Avalon Studios/Utils/Interfaces/IEvents.cs
@@ -0,0 +1,9 @@
+﻿namespace AvalonStudios.Additions.Utils.Interfaces
+{
+    public interface IEvents<in T>
+    {
+        void Subscribe(T listener);
+
+        void Unsubscribe(T listener);
+    }
+}
