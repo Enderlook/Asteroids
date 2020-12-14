d48e092fd25e07299b5d17c78ab74deaa07ddbe3
Enderlook <enderlook@gmail.com>
2020-12-14 00:07:41 -0300

    Prevent same power up spawning two times in a row

--- Assets/Scripts/Power Ups/PowerUpManager.cs
+++ Assets/Scripts/Power Ups/PowerUpManager.cs
@@ -29,6 +29,7 @@ namespace Asteroids.PowerUps
         private float cooldown;
         private AudioSource audioSource;
         private GameObject powerUp;
+        private int lastTemplate = -1;
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake()
@@ -78,7 +79,13 @@ namespace Asteroids.PowerUps
 
             Vector2 speed = (position - new Vector2(Random.value, Random.value)).normalized * this.speed.Value;
 
-            powerUp = templates.RandomPick().CreatePickup(audioSource, layer);
+            int index;
+            do
+            {
+                index = Random.Range(0, templates.Length);
+            } while (index == lastTemplate);
+
+            powerUp = templates[index].CreatePickup(audioSource, layer);
             powerUp.layer = layer;
             powerUp.transform.position = position; // Don't udpate from rigidbody because that has one frame delay
             powerUp.GetComponent<Rigidbody2D>().velocity = -speed;
